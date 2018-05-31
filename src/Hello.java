10dded6af1923266dcb7aaa2a91d41fcdc63484b
Mio <ywj1134915444@Gmail.com>
2018-05-31 13:06:25 +0800

    Hello

---
+++ src/Hello.java
@@ -0,0 +1,12 @@
+/**
+ * User: By Mio
+ * Name: PACKAGE_NAME.Hello
+ * Date: Created in 2018/5/31 12:55
+ */
+
+public class Hello {
+
+    public static void main(String[] str){
+        System.out.println("1");
+    }
+}
